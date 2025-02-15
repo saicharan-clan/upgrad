5f3af8ae269052f9050f19d73e00996d8d121729
saicharan-clan <ssaicharan0990@gmail.com>
2025-02-15 16:03:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ def home():
 @app.route('/health')
 def health():
   return jsonify({"status": "healthy"}), 200
-  if __name__ == '__main__'app.run(host='0.0.0.0', port=8000)
+  if __name__ == '__main__':
+      app.run(host='0.0.0.0', port=8000)
